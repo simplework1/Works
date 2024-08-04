abb5e1955396962f7d5cafafe8cb8c3d058d39d9
simplework1 <129606546+simplework1@users.noreply.github.com>
2024-08-05 02:56:44 +0530

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,15 +1,1 @@
-import re
 
-# Sample string
-text = 'My name is "Niladri Mullick".'
-
-# Regular expression to find text within quotes
-match = re.search(r'"([^"]*)"', text)
-
-# Extract the matched text if found
-if match:
-    quoted_text = match.group(1)
-else:
-    quoted_text = None
-
-print(quoted_text)
