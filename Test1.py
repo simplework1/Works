be3f3b396360b2229b8df64a4fb463ace3a3bce3
simplework1 <129606546+simplework1@users.noreply.github.com>
2024-08-05 16:10:34 +0530

    Update Test1.py

--- Test1.py
+++ Test1.py
@@ -1,15 +1,1 @@
-table = [
-    ["Name", "Viva Clothing ('the Firm')"],
-    ["Date of Establishment", "10 May 2019"],
-    ["Entity Type & Status", "Partnership Firm and Active"],
-    ["Brief Entity Profile", "According to the information obtained from the Partnership Deed of Viva Clothing dated 10 May 2019 provided by the Client, Viva Clothing is a Coimbatore, Tamil Nadu, India based partnership firm, established on 10 May 2019. The Firm was established with the objective of engaging in retail and wholesale of all types of garments, clothes, and accessories for various brands including 'The Ritu Kumar'"],
-    ["Business Addresses", [
-        "Globus Arcade, No.6, Race Course Road, Opposite To Vinayagar Temple, Coimbatore - 641011, Tamil Nadu, India",
-        "No. 88/4, Century Plaza, D.B. Road, R.S. Puram, Coimbatore - 641002, Tamil Nadu, India"
-    ]],
-    ["Permanent Account Number ('PAN')", "AARFV7428A"]
-]
 
-# Print the table
-for row in table:
-    print(row)
