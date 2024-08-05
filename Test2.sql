5a9ecc0fda0831e238970994ca2d3147ed9db47f
simplework1 <129606546+simplework1@users.noreply.github.com>
2024-08-06 01:03:31 +0530

    Update and rename Test2.py to Test2.sql

---
+++ Test2.sql
@@ -0,0 +1,5 @@
+INSERT INTO public."user" (
+    userid, username, firstname, lastname, createdOn, createdBy, modifiedOn, modifiedBy, emailID, userType
+) VALUES (
+    '1', 'jdoe', 'John', 'Doe', '2024-08-05 12:00:00', 'admin', '2024-08-05 12:00:00', 'admin', 'jdoe@example.com', 'standard'
+);
