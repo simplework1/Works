095e23e5870946023f7e3311d6eedccae6cdbb72
simplework1 <129606546+simplework1@users.noreply.github.com>
2024-08-12 00:24:18 +0530

    Update Test1.py

--- Test1.py
+++ Test1.py
@@ -1,9 +1,15 @@
 import pypandoc
 
-# Function to convert HTML to DOCX using Pandoc
+# Function to convert HTML with inline CSS to DOCX using Pandoc
 def html_to_docx(html_file_path, docx_file_path):
     # Convert HTML to DOCX using pypandoc
-    output = pypandoc.convert_file(html_file_path, 'docx', format='html', outputfile=docx_file_path)
+    output = pypandoc.convert_file(
+        source_file=html_file_path,
+        to='docx',
+        format='html',
+        outputfile=docx_file_path,
+        extra_args=['--standalone']
+    )
     print(f"Conversion complete: {docx_file_path}")
 
 # Paths to the input HTML and output DOCX
